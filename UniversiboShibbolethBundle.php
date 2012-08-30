c7f48ca97f92976ff05e34d2ab8d126f297a9026
Davide Bellettini <davide.bellettini@gmail.com>
2012-08-30 19:58:31 +0200

    Bundle class

---
+++ UniversiboShibbolethBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Universibo\Bundle\ShibbolethBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Davide Bellettini <davide.bellettini@studio.unibo.it>
+ */
+class UniversiboShibbolethBundle extends Bundle
+{
+}
+
