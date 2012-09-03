b01c95603cddf49fe1cdf60627bc20db81be59d1
Davide Bellettini <davide.bellettini@gmail.com>
2012-09-03 16:12:44 +0200

    Adding configuration options + adding zend uri dependency

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Universibo\Bundle\ShibbolethBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('universibo_shibboleth');
+
+        $rootNode
+            ->children()
+                ->arrayNode('idp_url')
+                    ->children()
+                        ->scalarNode('base')
+                            ->isRequired()
+                            ->cannotBeEmpty()
+                        ->end()
+                        ->scalarNode('info')
+                            ->isRequired()
+                            ->cannotBeEmpty()
+                        ->end()
+                        ->scalarNode('logout')
+                            ->isRequired()
+                            ->cannotBeEmpty()
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('route')
+                    ->children()
+                        ->scalarNode('after_logout')
+                            ->isRequired()
+                            ->cannotBeEmpty()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
