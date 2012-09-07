f38a53becc8d0aab764919d5fcd1b3902e33f03c
Davide Bellettini <davide.bellettini@gmail.com>
2012-09-07 15:29:43 +0200

    Adding Routing/Controller stub

---
+++ Controller/SSOController.php
@@ -0,0 +1,15 @@
+<?php
+namespace Universibo\Bundle\ShibbolethBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @author Davide Bellettini <davide.bellettini@gmail.com>
+ */
+class SSOController extends Controller 
+{
+    public function indexAction()
+    {
+        
+    }
+}
