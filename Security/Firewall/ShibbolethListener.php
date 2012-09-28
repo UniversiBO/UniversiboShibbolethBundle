848c42db7a4fd72b0c5fd22ced8df3ae27117dfa
Davide Bellettini <davide.bellettini@gmail.com>
2012-09-28 20:55:08 +0200

    Listener stub

--- Security/Firewall/ShibbolethListener.php
+++ Security/Firewall/ShibbolethListener.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Universibo\Bundle\ShibbolethBundle\Security\Firewall;
+use Universibo\Bundle\ShibbolethBundle\Security\Authentication\Token\ShibbolethToken;
+
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 use Symfony\Component\Security\Http\Firewall\ListenerInterface;
@@ -19,5 +21,10 @@ class ShibbolethListener implements ListenerInterface
     public function handle(GetResponseEvent $event)
     {
         $request = $event->getRequest();
+        
+        // checking if this page is secured by Shibboleth
+        if(is_null($sessionId = $request->server->get('Shib-Session-ID'))) {
+            return;
+        }
     }
 }
