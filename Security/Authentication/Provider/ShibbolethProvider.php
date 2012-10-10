896ff1426b3517cbe65816b208adf7af5421536b
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-10 17:51:04 +0200

    Implementing Auth Provider

--- Security/Authentication/Provider/ShibbolethProvider.php
+++ Security/Authentication/Provider/ShibbolethProvider.php
@@ -1,17 +1,33 @@
 <?php
 
 namespace Universibo\Bundle\ShibbolethBundle\Security\Authentication\Provider;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 use Universibo\Bundle\ShibbolethBundle\Security\Authentication\Token\ShibbolethToken;
-
+use Universibo\Bundle\ShibbolethBundle\Security\User\ShibbolethUserProviderInterface;
 use Symfony\Component\Security\Core\Authentication\Provider\AuthenticationProviderInterface;
+
 
 /**
  * @author Davide Bellettini <davide.bellettini@gmail.com>
  */
 class ShibbolethProvider implements AuthenticationProviderInterface
 {
+    /**
+     * @var ShibbolethUserProviderInterface
+     */
+    private $userProvider;
+    
+    public function __construct(ShibbolethUserProviderInterface $userProvider)
+    {
+        $this->userProvider = $userProvider;
+    }
+    
+    
     /**
      * (non-PHPdoc)
      * @see Symfony\Component\Security\Core\Authentication\Provider.AuthenticationProviderInterface::supports()
@@ -27,8 +43,17 @@ class ShibbolethProvider implements AuthenticationProviderInterface
      */
     public function authenticate(TokenInterface $token)
     {
-        // TODO: Auto-generated method stub
-
+        $user = $this->userProvider->loadUserByClaims($token->getClaims());
+        
+        // should never reach this
+        if(!$user instanceof UserInterface) {
+            throw new AuthenticationException('Shibboleth authentication Failed');
+        }
+        
+        $authenticatedToken = new ShibbolethToken($user->getRoles());
+        $authenticatedToken->setUser($user);
+        
+        return $authenticatedToken;
     }
 
 }
