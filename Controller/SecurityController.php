95a93f5e6e3d93547bf5ea0b1b63c7323227b34b
Davide Bellettini <davide.bellettini@gmail.com>
2012-10-11 01:05:32 +0200

    Security controller

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -10,5 +10,15 @@ class SecurityController extends Controller
 {
     public function loginAction()
     {
+        $request = $this->getRequest();
+        
+        $url = $request->headers->get('Referer');
+        $this->redirect($url);
+        
+        // TODO method stub
+    }
+    
+    public function logoutAction()
+    {
     }
 }
