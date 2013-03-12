ba8f9ea8386b326d37198001eccc3266a7e94db4
Davide Bellettini <davide.bellettini@gmail.com>
2013-03-13 00:11:17 +0100

    Fixing uses

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Security\Http\Logout\LogoutHandlerInterface;
